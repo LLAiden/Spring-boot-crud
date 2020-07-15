5afa95e38edd27673d9fab74e81e27de883cb162
Aiden <LLAiden@qq.com>
2020-07-15 23:58:47 +0800

    添加页面显示部门

--- src/main/java/com/aiden/sringbootrestful/controller/EmployeeController.java
+++ src/main/java/com/aiden/sringbootrestful/controller/EmployeeController.java
@@ -1,11 +1,13 @@
 package com.aiden.sringbootrestful.controller;
 
+import com.aiden.sringbootrestful.dao.DepartmentDao;
 import com.aiden.sringbootrestful.dao.EmployeeDao;
+import com.aiden.sringbootrestful.entities.Department;
 import com.aiden.sringbootrestful.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Collection;
 
@@ -15,10 +17,21 @@ public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
 
-    @RequestMapping("/empList")
+    @Autowired
+    DepartmentDao departmentDao;
+
+    @GetMapping("/empList")
     public String list(Model model) {
         Collection<Employee> all = employeeDao.getAll();
         model.addAttribute("empList", all);
         return "emp/list";
+    }
+
+    @GetMapping("/emp")
+    public String toAddPage(Model model) {
+        System.out.println("进入添加界面");
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departmentList",departments);
+        return "emp/add";
     }
 }
